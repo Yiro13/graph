921b52096863b9f5ba1744ba90bdbe3c34c1a7c9
Yahir Leo <yahir.banuelos5763@alumnos.udg.mx>
2023-08-23 13:59:08 -0600

    delete node - added, delete edge added, show options added

--- grafo.cpp
+++ grafo.cpp
@@ -62,7 +62,6 @@ public:
     void insertarNodo(Nodo* nodo){
         if(!header){
             header = nodo;
-            std::cout<<"PRIMER NODO"<<std::endl;
         }else{
             Nodo* aux = header;
             while(aux->siguiente != nullptr){
@@ -79,9 +78,26 @@ public:
             Arista* auxArista = aux->adyacenciaH;
             Arista* auxAristaAnterior = auxArista;
             while(auxArista != nullptr){
+                if((auxArista->siguiente == nullptr) && (auxArista->destino->nombre == nombre) && (auxArista != aux->adyacenciaH)){
+                    auxAristaAnterior->siguiente = auxArista->siguiente;
+                    //delete auxArista;
+                }else if(auxArista->destino->nombre == nombre && auxArista == aux->adyacenciaH){
+                    aux->adyacenciaH = aux->adyacenciaH->siguiente;
+                    //delete auxArista;
+                }else if(auxArista->destino->nombre == nombre){
+                    auxAristaAnterior->siguiente = auxArista->siguiente;
+                    //delete auxArista;
+                }
                 
                 auxAristaAnterior = auxArista;
                 auxArista = auxArista->siguiente;
+            }
+            if((aux->siguiente == nullptr) && (aux->nombre == nombre) && (aux != header)){
+                auxAnterior->siguiente = aux->siguiente;
+            }else if(aux->nombre == nombre && aux == header){
+                header = header->siguiente;
+            }else if(aux->nombre == nombre){
+                auxAnterior->siguiente = aux->siguiente;
             }
             
             auxAnterior = aux;
@@ -90,7 +106,28 @@ public:
     }
 
     void eliminarArista(std::string origen, std::string destino){
-
+        Nodo* aux = header;
+        Nodo* auxAnterior = aux;
+        while(aux){
+            Arista* auxArista = aux->adyacenciaH;
+            Arista* auxAristaAnterior = auxArista;
+            while(auxArista){
+                if((auxArista->siguiente == nullptr) && (auxArista != aux->adyacenciaH) && ((auxArista->origen->nombre == origen) && (auxArista->destino->nombre == destino))){
+                    auxAristaAnterior->siguiente = auxArista->siguiente;
+                    //delete auxArista;
+                }else if(((auxArista->origen->nombre == origen) && (auxArista->destino->nombre == destino)) && auxArista == aux->adyacenciaH){
+                    aux->adyacenciaH = aux->adyacenciaH->siguiente;
+                    //delete auxArista;
+                }else if(auxArista->origen->nombre == origen && auxArista->destino->nombre == destino){
+                    auxAristaAnterior->siguiente = auxArista->siguiente;
+                    //delete auxArista;
+                }
+                auxAristaAnterior = auxArista;
+                auxArista = auxArista->siguiente;
+            }
+            auxAnterior = aux;
+            aux = aux->siguiente;
+        }
     }
 
     void insertarArista(Arista* arista){
@@ -104,7 +141,6 @@ public:
         Nodo* encontrado = buscarNodo(arista->origen->nombre);
         if(!encontrado->adyacenciaH){
             encontrado->adyacenciaH = arista;
-            std::cout<<"PRIMER ARISTA"<<std::endl; 
         }else{
             Arista* aux = encontrado->adyacenciaH;
             while(aux->siguiente != nullptr){
@@ -116,21 +152,36 @@ public:
 
     void mostrarGrafo(void){
         Nodo* aux = header;
-            while(aux){
-                Arista* auxArista = aux->adyacenciaH;
-                std::cout<<aux->nombre + "\t|";
-                while(auxArista){
-                    std::cout<<auxArista->destino->nombre + " ";
-                    auxArista = auxArista->siguiente;
-                }
-                aux = aux->siguiente;
-                std::cout<<std::endl;
+        std::cout<<"NODOS \t|LISTAS DE ADYACENCIA"<<std::endl;
+        std::cout<<"--------+--------------------"<<std::endl;
+        while(aux){
+            Arista* auxArista = aux->adyacenciaH;
+            std::cout<<aux->nombre + "\t|";
+            while(auxArista){
+                std::cout<<auxArista->destino->nombre + " ";
+                auxArista = auxArista->siguiente;
             }
+            aux = aux->siguiente;
+            std::cout<<std::endl;
+        }
+        std::cout<<std::endl;
+    }
+
+    void mostrarAristas(void){
+        Nodo* aux = header;
+        while(aux){
+            Arista* auxArista = aux->adyacenciaH;
+            while(auxArista){
+                std::cout<<auxArista->origen->nombre<<" ------------> "<<auxArista->destino->nombre<<std::endl;
+                auxArista = auxArista->siguiente;
+            }
+            aux = aux->siguiente;
+        }
     }
 };
  
 int main(){
-    Grafo* a = new Grafo;
+    /*Grafo* a = new Grafo;
     Nodo* b = new Nodo();
     b->nombre = "A";
     Nodo* c = new Nodo();
@@ -144,18 +195,57 @@ int main(){
 
     Arista* aris = new Arista;
     aris->origen = b;
-    aris->destino = c;
+    aris->destino = d;
+
+    Arista* aris3 = new Arista;
+    aris3->origen = b;
+    aris3->destino = c;
+
+    Arista* aris5 = new Arista;
+    aris5->origen = b;
+    aris5->destino = b;
+
+    Arista* aris4 = new Arista;
+    aris4->origen = b;
+    aris4->destino = b;
 
     Arista* aris2 = new Arista;
     aris2->origen = c;
     aris2->destino = b;
 
-    a->insertarArista(aris);
-    a->insertarArista(aris2);
-    a->mostrarGrafo();
-    a->eliminarNodo("A");
+    Arista* aris6 = new Arista;
+    aris6->origen = c;
+    aris6->destino = d;
 
-    a->mostrarGrafo();
+    Arista* aris7 = new Arista;
+    aris7->origen = d;
+    aris7->destino = d; 
+
+    a->insertarArista(aris4);
+    a->insertarArista(aris);
+    a->insertarArista(aris3);
+    a->insertarArista(aris2);
+    a->insertarArista(aris5);
+    a->insertarArista(aris6);
+    a->insertarArista(aris7);
+    
+
+    a->mostrarAristas();*/
+    int opc = 0;
+
+    while(opc != 7){
+        system("cls");
+        std::cout<<"\n\t GRAFOS DIRIGIDO \n\n";
+        std::cout<<" 1. INSERTAR UN NODO "<<std::endl;
+        std::cout<<" 2. INSERTAR UNA ARISTA "<<std::endl;
+        std::cout<<" 3. ELIMINAR UN NODO "<<std::endl;
+        std::cout<<" 4. ELIMINAR UNA ARISTA "<<std::endl;
+        std::cout<<" 5. MOSTRAR GRAFO "<<std::endl;
+        std::cout<<" 6. MOSTRAR ARISTAS DE UN NODO "<<std::endl;
+        std::cout<<" 7. SALIR "<<std::endl;
+        std::cout<<"\n INGRESE OPCION: ";
+        std::cin>>opc;
+    }
 
 
     return 0;
